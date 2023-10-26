fc1348cac7e595fdc2542c62ec0566a54100e30c
Robert <robertwachirae@gmail.com>
2023-10-26 15:46:35 +0300

    Pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -4,17 +4,8 @@ import PricingCard from "./pricing-card";
 import styles from "../page.module.css";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
-import { useEffect, useState } from "react";
-// import PricinCard from "../../../components/pricingcomponents/creditprice";
-import data from "../../../components/pricingcomponents/credits.json";
 
-export default function Pricing({ props }) {
-  // const [creditings, setCreditings] = useState({
-  //   credits: ["50", "100", "250"],
-  //   description: ["50 Credits", "100 Credits", "250 Credits"],
-  //   quantity: 1,
-  //   price: ["9", "19", "29"],
-  // });
+export default function Pricing() {
   return (
     <>
       <div className={styles.divmain}>
